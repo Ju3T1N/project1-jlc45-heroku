c9c3e7419e56e946d7d2d2ec6654ea72200d83c6
EC2 Default User <ec2-user@ip-172-31-30-207.ec2.internal>
2020-09-18 04:08:33 +0000

    Created file and a list of foods to find recipes for on startup

---
+++ p1.py
@@ -0,0 +1,7 @@
+import random
+
+#choose food from a list of foods at least for intialization
+foodlist=['pork','pie','pizza','curry', 'steak','jumbo','spaghetti']
+cfood=random.randint(0,6)
+
+print (foodlist[cfood])
