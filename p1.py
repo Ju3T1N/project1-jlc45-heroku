0a39cb8f20795bcded9172908dc30818eaea4746
EC2 Default User <ec2-user@ip-172-31-30-207.ec2.internal>
2020-09-18 19:56:42 +0000

    added calls to twitter api to get random tweets of food item

--- p1.py
+++ p1.py
@@ -1,7 +1,24 @@
+from tweepy import OAuthHandler
+from tweepy import API
 import random
+import os
 
-#choose food from a list of foods at least for intialization
-foodlist=['pork','pie','pizza','curry', 'steak','jumbo','spaghetti']
-cfood=random.randint(0,6)
 
-print (foodlist[cfood])
+foodlist=['pork chop','cookie','pizza','curry', 'steak','jumbo shrimp','spaghetti'] #list
+random.seed() #initialize rng
+cfood=foodlist[random.randint(0,6)] #use random to choose a food
+
+consumer_key=os.environ['TKEY']
+consumer_secret=os.environ['TSKEY']
+access_token=os.environ['TACC']
+access_token_secret=os.environ['TSACC']
+
+tauth = OAuthHandler(consumer_key , consumer_secret)
+tauth.set_access_token(access_token , access_token_secret)
+twitter_api = API(tauth)
+
+
+tweets=twitter_api.search(cfood, count=50)
+tweetlist=[]
+for tweet in tweets:
+    tweetlist.append([tweet.user.screen_name, str(tweet.created_at), tweet.text])
